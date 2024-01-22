ac71cbb25a3c5e5e8698028882bda71fa6b3f9de
carladavesa <carladavesa4@gmail.com>
2024-01-22 16:34:47 +0000

    Replace values for int parameters

--- unified_planning/engines/compilers/int_action_remover.py
+++ unified_planning/engines/compilers/int_action_remover.py
@@ -146,9 +146,9 @@ class IntActionRemover(engines.engine.Engine, CompilerMixin):
         for key in int_parameters.keys():
             if key.split('] ')[1] in str(new_value):
                 new_value = c[int_parameters.get(key)]
-        print(type(new_value))
+        print(type(new_value.constant_value()))
         print(new_value.constant_value())
-        return new_value()
+        return new_value.constant_value()
 
     def _get_new_fnode(
             self,
