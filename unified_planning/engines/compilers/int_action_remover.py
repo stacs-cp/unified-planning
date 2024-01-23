8ac9e58a5075342ab98d5e059d9432fc5fa1e600
carladavesa <carladavesa4@gmail.com>
2024-01-23 12:05:27 +0000

    Added prints

--- unified_planning/engines/compilers/int_action_remover.py
+++ unified_planning/engines/compilers/int_action_remover.py
@@ -144,11 +144,12 @@ class IntActionRemover(engines.engine.Engine, CompilerMixin):
     ) -> "up.model.expression.Expression":
         new_value = value
         print(value.node_type)
+        print(value.args)
         print(value.environment)
         for key in int_parameters.keys():
             if key.split('] ')[1] in str(new_value):
                 new_value = c[int_parameters.get(key)]
-        return FNode(new_value, value.node_id, value.environment)
+        return new_value
 
     def _get_new_fnode(
             self,
