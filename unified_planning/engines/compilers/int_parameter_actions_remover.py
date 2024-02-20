d0db4ef9102c4ac6d881f4b471b246ae13e6330b
carladavesa <carladavesa4@gmail.com>
2024-02-20 14:13:22 +0000

    Changes in IntParameterActionsRemover

--- unified_planning/engines/compilers/int_parameter_actions_remover.py
+++ unified_planning/engines/compilers/int_parameter_actions_remover.py
@@ -206,11 +206,7 @@ class IntParameterActionsRemover(engines.engine.Engine, CompilerMixin):
                     int_domains.append(domain)
             combinations = list(product(*int_domains))
             for c in combinations:
-                cadena = '_'.join(map(str, c))
-                new_action = action.clone()
-                new_action.name = action.name + '_' + cadena
-                new_action.clear_preconditions()
-                new_action.clear_effects()
+                new_action = InstantaneousAction(action.name + '_' + '_'.join(map(str, c)), new_parameters, action.environment)
                 for precondition in action.preconditions:
                     new_precondition = self._manage_node(em, precondition, int_parameters, c)
                     new_action.add_precondition(new_precondition)
