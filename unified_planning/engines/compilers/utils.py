dd157d0b6daec15cc8c6c8ea67fc6f3a381881a9
carladavesa <carladavesa4@gmail.com>
2024-03-11 11:44:11 +0000

    Prints added

--- unified_planning/engines/compilers/utils.py
+++ unified_planning/engines/compilers/utils.py
@@ -279,9 +279,9 @@ def lift_action_instance(
 
     Where the grounded action is obtained by grounding
     the "original_action" with the specific "parameters"."""
-    print("busquem: ", action_instance.action.name)
+    print("busquem: ", action_instance.action.name, action_instance.action.parameters)
     lifted_action, parameters = map[action_instance.action]
-    print("trobem: ",lifted_action.name, parameters)
+    print("trobem: ", lifted_action.name, parameters)
     return ActionInstance(lifted_action, tuple(parameters))
 
 
