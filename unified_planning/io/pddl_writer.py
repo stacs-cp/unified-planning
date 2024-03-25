4126993546862bcba778bc318e5e58ee662a9543
carladavesa <carladavesa4@gmail.com>
2024-03-25 11:40:31 +0000

    Changes in new expression created

--- unified_planning/io/pddl_writer.py
+++ unified_planning/io/pddl_writer.py
@@ -540,10 +540,10 @@ class PDDLWriter:
         out.write(
             f' (:functions {" ".join(functions)})\n' if len(functions) > 0 else ""
         )
-
         converter = ConverterToPDDLString(
             self.problem.environment, self._get_mangled_name
         )
+        print(converter.count_functions)
         costs = {}
         metrics = self.problem.quality_metrics
         if len(metrics) == 1:
@@ -782,7 +782,6 @@ class PDDLWriter:
                 goals_str.extend(map(converter.convert, g.args))
             else:
                 goals_str.append(converter.convert(g))
-        print(converter.count_functions)
         out.write(f' (:goal (and {" ".join(goals_str)}))\n')
         if len(self.problem.trajectory_constraints) > 0:
             out.write(
