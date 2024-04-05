6b7f434899e4fec710e7d731b68b9f71dd1c99cf
carladavesa <carladavesa4@gmail.com>
2024-04-05 11:23:55 +0100

    Changes in walk_equals expression to not accept boolean arrays

--- unified_planning/model/walkers/type_checker.py
+++ unified_planning/model/walkers/type_checker.py
@@ -84,6 +84,7 @@ class TypeChecker(walkers.dag.DagWalker):
         :param expression: The expression of which the `Type` must be retrieved.
         :return: The expression `Type`.
         """
+        print(expression)
         res = self.walk(expression)
         if res is None:
             raise UPTypeError(
@@ -113,6 +114,7 @@ class TypeChecker(walkers.dag.DagWalker):
     def walk_iff(
         self, expression: FNode, args: List["unified_planning.model.types.Type"]
     ) -> Optional["unified_planning.model.types.Type"]:
+        print(args)
         assert expression is not None
         t = args[0]
         if t.is_array_type():
