cff0539a14e195c880e11bdd0365ac946d7dfd24
carladavesa <carladavesa4@gmail.com>
2024-06-18 15:41:42 +0100

    Restored

--- unified_planning/engines/compilers/count_remover.py
+++ unified_planning/engines/compilers/count_remover.py
@@ -63,7 +63,6 @@ class CountRemover(engines.engine.Engine, CompilerMixin):
     def supported_kind() -> ProblemKind:
         supported_kind = ProblemKind(version=LATEST_PROBLEM_KIND_VERSION)
         supported_kind.set_problem_class("ACTION_BASED")
-        supported_kind.set_problem_class("HIERARCHICAL")
         supported_kind.set_typing("FLAT_TYPING")
         supported_kind.set_typing("HIERARCHICAL_TYPING")
         supported_kind.set_parameters("BOOL_FLUENT_PARAMETERS")
