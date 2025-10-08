0eb1c95d5435b001e27224831ae500b465c63f16
Carla <carladavesa4@gmail.com>
2025-10-08 11:19:07 +0100

    New compiler to add control for bounded integers added.

---
+++ unified_planning/engines/compilers/unboundedness_remover.py
@@ -0,0 +1,203 @@
+# Copyright 2021-2023 AIPlan4EU project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+"""This module defines the unboundedness remover class."""
+
+
+import unified_planning as up
+import unified_planning.engines as engines
+from unified_planning.engines.mixins.compiler import CompilationKind, CompilerMixin
+from unified_planning.engines.results import CompilerResult
+from unified_planning.model import Problem, ProblemKind, Fluent, FNode, Action
+from unified_planning.model.fluent import get_all_fluent_exp
+from unified_planning.model.types import _RealType, _IntType
+from unified_planning.model.problem_kind_versioning import LATEST_PROBLEM_KIND_VERSION
+from unified_planning.model.walkers import FluentsSubstituter
+from unified_planning.engines.compilers.utils import (
+    add_invariant_condition_apply_function_to_problem_expressions,
+    replace_action, get_fresh_name,
+)
+from typing import List, Dict, OrderedDict, Optional, Union, cast
+from functools import partial
+
+from unified_planning.shortcuts import LE, GE, And
+
+
+class UnboundednessRemover(engines.engine.Engine, CompilerMixin):
+    """
+    Bounded types remover class: this class offers the capability
+    to transform a :class:`~unified_planning.model.Problem` with Bounded :class:`Types <unified_planning.model.Type>`
+    into a `Problem` without bounded `Types` (only IntType and RealType can be bounded).
+    This capability is offered by the :meth:`~unified_planning.engines.compilers.BoundedTypesRemover.compile`
+    method, that returns a :class:`~unified_planning.engines.CompilerResult` in which the :meth:`problem <unified_planning.engines.CompilerResult.problem>` field
+    is the compiled Problem.
+
+    This is done by changing the type of the fluents to unbounded types, and adding to every action's condition and
+    every goal of the problem the artificial condition that emulates the typing bound.
+
+    For example, if we have a fluent `F` of type `int[0, 5]`, the added condition would be `0 <= F <= 5`.
+
+    This `Compiler` supports only the the `BOUNDED_TYPES_REMOVING` :class:`~unified_planning.engines.CompilationKind`.
+    """
+
+    def __init__(self):
+        engines.engine.Engine.__init__(self)
+        CompilerMixin.__init__(self, CompilationKind.UNBOUNDEDNESS_REMOVING)
+
+    @property
+    def name(self):
+        return "ubr"
+
+    @staticmethod
+    def supported_kind() -> ProblemKind:
+        supported_kind = ProblemKind(version=LATEST_PROBLEM_KIND_VERSION)
+        supported_kind.set_problem_class("ACTION_BASED")
+        supported_kind.set_typing("FLAT_TYPING")
+        supported_kind.set_typing("HIERARCHICAL_TYPING")
+        supported_kind.set_parameters("BOOL_FLUENT_PARAMETERS")
+        supported_kind.set_parameters("BOUNDED_INT_FLUENT_PARAMETERS")
+        supported_kind.set_parameters("BOOL_ACTION_PARAMETERS")
+        supported_kind.set_parameters("BOUNDED_INT_ACTION_PARAMETERS")
+        supported_kind.set_parameters("UNBOUNDED_INT_ACTION_PARAMETERS")
+        supported_kind.set_parameters("REAL_ACTION_PARAMETERS")
+        supported_kind.set_numbers("BOUNDED_TYPES")
+        supported_kind.set_problem_type("SIMPLE_NUMERIC_PLANNING")
+        supported_kind.set_problem_type("GENERAL_NUMERIC_PLANNING")
+        supported_kind.set_fluents_type("INT_FLUENTS")
+        supported_kind.set_fluents_type("REAL_FLUENTS")
+        supported_kind.set_fluents_type("OBJECT_FLUENTS")
+        supported_kind.set_conditions_kind("NEGATIVE_CONDITIONS")
+        supported_kind.set_conditions_kind("DISJUNCTIVE_CONDITIONS")
+        supported_kind.set_conditions_kind("EQUALITIES")
+        supported_kind.set_conditions_kind("EXISTENTIAL_CONDITIONS")
+        supported_kind.set_conditions_kind("UNIVERSAL_CONDITIONS")
+        supported_kind.set_effects_kind("CONDITIONAL_EFFECTS")
+        supported_kind.set_effects_kind("INCREASE_EFFECTS")
+        supported_kind.set_effects_kind("DECREASE_EFFECTS")
+        supported_kind.set_effects_kind("STATIC_FLUENTS_IN_BOOLEAN_ASSIGNMENTS")
+        supported_kind.set_effects_kind("STATIC_FLUENTS_IN_NUMERIC_ASSIGNMENTS")
+        supported_kind.set_effects_kind("STATIC_FLUENTS_IN_OBJECT_ASSIGNMENTS")
+        supported_kind.set_effects_kind("FLUENTS_IN_BOOLEAN_ASSIGNMENTS")
+        supported_kind.set_effects_kind("FLUENTS_IN_NUMERIC_ASSIGNMENTS")
+        supported_kind.set_effects_kind("FLUENTS_IN_OBJECT_ASSIGNMENTS")
+        supported_kind.set_effects_kind("FORALL_EFFECTS")
+        supported_kind.set_time("CONTINUOUS_TIME")
+        supported_kind.set_time("DISCRETE_TIME")
+        supported_kind.set_time("INTERMEDIATE_CONDITIONS_AND_EFFECTS")
+        supported_kind.set_time("EXTERNAL_CONDITIONS_AND_EFFECTS")
+        supported_kind.set_time("TIMED_EFFECTS")
+        supported_kind.set_time("TIMED_GOALS")
+        supported_kind.set_time("DURATION_INEQUALITIES")
+        supported_kind.set_time("SELF_OVERLAPPING")
+        supported_kind.set_expression_duration("STATIC_FLUENTS_IN_DURATIONS")
+        supported_kind.set_expression_duration("FLUENTS_IN_DURATIONS")
+        supported_kind.set_expression_duration("INT_TYPE_DURATIONS")
+        supported_kind.set_expression_duration("REAL_TYPE_DURATIONS")
+        supported_kind.set_simulated_entities("SIMULATED_EFFECTS")
+        supported_kind.set_constraints_kind("STATE_INVARIANTS")
+        supported_kind.set_quality_metrics("ACTIONS_COST")
+        supported_kind.set_actions_cost_kind("STATIC_FLUENTS_IN_ACTIONS_COST")
+        supported_kind.set_actions_cost_kind("FLUENTS_IN_ACTIONS_COST")
+        supported_kind.set_quality_metrics("PLAN_LENGTH")
+        supported_kind.set_quality_metrics("OVERSUBSCRIPTION")
+        supported_kind.set_quality_metrics("TEMPORAL_OVERSUBSCRIPTION")
+        supported_kind.set_quality_metrics("MAKESPAN")
+        supported_kind.set_quality_metrics("FINAL_VALUE")
+        supported_kind.set_actions_cost_kind("INT_NUMBERS_IN_ACTIONS_COST")
+        supported_kind.set_actions_cost_kind("REAL_NUMBERS_IN_ACTIONS_COST")
+        supported_kind.set_oversubscription_kind("INT_NUMBERS_IN_OVERSUBSCRIPTION")
+        supported_kind.set_oversubscription_kind("REAL_NUMBERS_IN_OVERSUBSCRIPTION")
+        return supported_kind
+
+    @staticmethod
+    def supports(problem_kind):
+        return problem_kind <= UnboundednessRemover.supported_kind()
+
+    @staticmethod
+    def supports_compilation(compilation_kind: CompilationKind) -> bool:
+        return compilation_kind == CompilationKind.UNBOUNDEDNESS_REMOVING
+
+    @staticmethod
+    def resulting_problem_kind(
+        problem_kind: ProblemKind, compilation_kind: Optional[CompilationKind] = None
+    ) -> ProblemKind:
+        new_kind = problem_kind.clone()
+        return new_kind
+
+    def _compile(
+        self,
+        problem: "up.model.AbstractProblem",
+        compilation_kind: "up.engines.CompilationKind",
+    ) -> CompilerResult:
+        """
+        Takes an instance of a :class:`~unified_planning.model.Problem` and the wanted :class:`~unified_planning.engines.CompilationKind`
+        and returns a :class:`~unified_planning.engines.results.CompilerResult` where the :meth:`problem<unified_planning.engines.results.CompilerResult.problem>`
+        field controls that the bounded integer fluents remain inside the bounds.
+
+        :param problem: The instance of the :class:`~unified_planning.model.Problem` that must be returned with checks for the bounds.
+        :param compilation_kind: The :class:`~unified_planning.engines.CompilationKind` that must be applied on the given problem;
+            only :class:`~unified_planning.engines.CompilationKind.UNBOUNDEDNESS_REMOVING` is supported by this compiler
+        :return: The resulting :class:`~unified_planning.engines.results.CompilerResult` data structure.
+        """
+        assert isinstance(problem, Problem)
+
+        new_to_old: Dict[Action, Action] = {}
+
+        new_problem = problem.clone()
+        new_problem.name = f"{self.name}_{problem.name}"
+        new_problem.clear_actions()
+        new_problem.clear_quality_metrics()
+        env = new_problem.environment
+        tm = env.type_manager
+
+        bounded_fluents = {}
+
+        # Fluents
+        for fluent in problem.fluents:
+            if fluent.type.is_int_type() and fluent.type.lower_bound is not None and fluent.type.upper_bound is not None:
+                bounded_fluents[fluent] = (fluent.type.lower_bound, fluent.type.upper_bound)
+
+        print(bounded_fluents)
+
+        # Actions
+        for action in problem.actions:
+            remove_action = False
+            new_action = action.clone()
+            new_action.name = get_fresh_name(new_problem, action.name)
+            new_action.clear_preconditions()
+            new_action.clear_effects()
+            for precondition in action.preconditions:
+                new_action.add_precondition(precondition)
+            for effect in action.effects:
+                fluent = effect.fluent.fluent()
+                if fluent in bounded_fluents.keys():
+                    lb, ub = fluent.type.lower_bound, fluent.type.upper_bound
+                    if effect.condition is True:
+                        new_action.add_precondition(And(LE(effect.value, ub), GE(effect.value, lb)).simplify())
+                    else:
+                        new_action.add_precondition(And(effect.condition, GE(effect.value, lb), LE(effect.value, ub)).simplify())
+
+                if effect.is_increase():
+                    new_action.add_increase_effect(effect.fluent, effect.value, effect.condition, effect.forall)
+                elif effect.is_decrease():
+                    new_action.add_decrease_effect(effect.fluent, effect.value, effect.condition, effect.forall)
+                else:
+                    new_action.add_effect(effect.fluent, effect.value, effect.condition, effect.forall)
+
+            new_to_old[new_action] = action
+            new_problem.add_action(new_action)
+
+        return CompilerResult(
+            new_problem, partial(replace_action, map=new_to_old), self.name
+        )
--- unified_planning/engines/factory.py
+++ unified_planning/engines/factory.py
@@ -75,6 +75,10 @@ DEFAULT_ENGINES = {
         "unified_planning.engines.compilers.bounded_types_remover",
         "BoundedTypesRemover",
     ),
+    "up_unboundedness_remover": (
+        "unified_planning.engines.compilers.unboundedness_remover",
+        "UnboundednessRemover",
+    ),
     "up_conditional_effects_remover": (
         "unified_planning.engines.compilers.conditional_effects_remover",
         "ConditionalEffectsRemover",
@@ -173,6 +177,7 @@ DEFAULT_ENGINES_PREFERENCE_LIST = [
     "sequential_simulator",
     "up_time_triggered_validator",
     "up_bounded_types_remover",
+    "up_unboundedness_remover",
     "up_conditional_effects_remover",
     "up_disjunctive_conditions_remover",
     "up_ma_disjunctive_conditions_remover",
